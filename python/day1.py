b584e9d1b269b62d62932ffc1e4a4ca4b4434f42
monisha4evr <monisha.it1510@gmail.com>
2026-06-12 17:34:37 +0530

    python variables , data types, conversion

---
+++ python/day1.py
@@ -0,0 +1,79 @@
+
+data type: 
+
+Number
+number - 10  - int
+decimal - 15.3 - float
+
+
+None
+boolean - True/False  - bool
+
+sequence
+string - "Apple" - str
+list 
+tuple  immutable
+set
+
+dict {k=>v} 
+
+a= {1,2,3,4,5}
+
+
+mutable - can change
+immutable - can't change
+
+ordered - 
+unordered - 
+
+
+type()
+
+a=10
+print(type(a))
+
+b=10.3
+print(type(b))
+
+
+c="Flower"
+print(type(c))
+
+
+id()
+
+a=10
+b=10
+
+print(id(a),id(b))
+
+a=[1,2,3,4]
+b=[1,2,3,4,5]
+
+print(id(a),id(b))
+
+
+a="apple"
+b="apple"
+print(id(a),id(b))
+
+
+type conversion:
+
+1.implicit ( Automatic)
+2.explicit ( manual )
+
+a=10
+b=10.5
+c=a+b
+print(type(a))
+print(type(b))
+print(type(c))
+
+a=int("123")
+b=45
+print(a+b)
+
+Task :
+a=input("Enter a value")
+a=int(input("enter a value"))
--- python/sample.py
+++
@@ -1,3 +0,0 @@
-
-a=input("Enter a value")
-print(a)
