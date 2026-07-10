00690f2b50149c5c572b5a2332e64de3501b80ed
monisha4evr <monisha.it1510@gmail.com>
2026-07-10 16:21:55 +0530

    javascript

--- javascript/intro.js
+++ javascript/intro.js
@@ -46,8 +46,75 @@ const :
 4. Need initialization at Declaration time
 5. support but put in TDZ
 
+Temporal Dead Zone.
 
-hoisting:
-var a;
-console.log(a);
-var a=10;
+const a=10;
+console.log("Outer",a);
+function fname(){
+    console.log("Inside Function",a);
+    if(1){
+        console.log("Inside Block",a)
+    }
+}
+fname();
+console.log("Outer function",a);
+
+
+function test(){
+    let a=10;
+    console.log("inside Function",a)
+    if(1){
+        console.log("Inside the Block",a)
+    }
+
+}
+test()
+console.log("outside the Funciton",a)
+
+function sample(){
+    for(let i=1;i<=5;i++){
+        console.log("Inside",i)
+    }
+    console.log("Outside",i)
+}
+sample()
+
+Operator:
+----------
+1. Arithmetic(+,-,*,/,%,++,--)
+    pre increment/post increment
+
+    a=5
+    a++ // POST: 5 - 6 - assign then Increment
+   a=5=> ++a // PRE : 6 increment - asign
+
+   a=5
+   console.log(++a)
+   console.log(a)
+
+   b=5
+   b++
+   ++b
+   console.log(b++)
+   console.log(b)
+
+
+   c=15
+   c++
+   console.log(++c)
+   console.log(++c)
+   console.log(c++)
+   ++c
+    console.log(c)
+
+2. Assignment (+=,=,-=,*=,/=)
+3. Comparision(==,>=,<=,>,<,===,!===,!=)
+    console.log('5'==5)
+    console.log(5==5)
+    console.log(5===5)
+    console.log('5'===5)
+
+4. Logical (&& || !)
+
+console.log(5+4+"3")
+console.log("3"+5+4)
